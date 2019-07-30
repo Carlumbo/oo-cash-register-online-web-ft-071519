370730824ac7a68903b9fcee8b36a0ef88e80c3a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-30 17:12:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,16 @@
 class CashRegister
 attr_accessor :total
-attr_reader :discount
+attr_reader :discount, :items 
 
   def initialize(discount = 0 )
     @total = 0
     @discount = discount
+    @items = []
   end
 
 def add_item(title, price, quantity = 1)
   @total += price * quantity
-
+  @items << title
 end
 
 def apply_discount
