9a94b1f0500b27d6855afdfde5c6e72dd78a7d6f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-30 17:14:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,19 +8,22 @@ attr_reader :discount, :items
     @items = []
   end
 
-def add_item(title, price, quantity = 1)
-  @total += price * quantity
-  @items << title
-end
-
-def apply_discount
-  @total = @total - @total * @discount / 100
-  if @discount != 0
-    "After the discount, the total comes to $#{total}."
-  else
-    "There is no discount to apply."
-
+  def add_item(title, price, quantity = 1)
+   quantity.times do
+     @items << title
+   else
+    @total += price * quantity
+    end
   end
+
+  def apply_discount
+    @total = @total - @total * @discount / 100
+    if @discount != 0
+      "After the discount, the total comes to $#{total}."
+    else
+      "There is no discount to apply."
+
+    end
 end
 
 
