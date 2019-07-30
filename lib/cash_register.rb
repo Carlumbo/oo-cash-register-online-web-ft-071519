866745ff46c73ef2759cd36d7eed64789a21d990
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-30 17:05:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,8 @@ def add_item(title, price, quantity = 1)
 end
 
 def apply_discount
-  @total -= @total * @discount / 100
   if @discount != 0
+    @total -= @total * @discount / 100
     "After the discount, the total comes to $#{@total}."
   else
    "There is no discount to apply"
