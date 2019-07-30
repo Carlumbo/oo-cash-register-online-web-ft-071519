654689fbc2c41a32ff3d77ea4f69a81fdfcc8066
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-30 16:48:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,9 @@ def add_item(title, price, quantity = 1)
 end
 
 def apply_discount
-  @total = @total - @total * @discount / 100
-  "After the discount, the total comes to $800."
+  @total -= @total * @discount / 100
+  "After the discount, the total comes to $#{@total}."
+  
 end
 
 
